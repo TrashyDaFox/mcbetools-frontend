c288bae01346315ceb63a1450773eba8d478d74f
Ant767 <antspa767@gmail.com>
2024-11-09 20:28:14 -0600

    UI Improvements

--- src/routes/(main)/ProjectCard.svelte
+++ src/routes/(main)/ProjectCard.svelte
@@ -174,7 +174,7 @@
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: currentColor;"><path d="M17 8V7c0-2.757-2.243-5-5-5S7 4.243 7 7v3H6c-1.103 0-2 .897-2 2v8c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-8c0-1.103-.897-2-2-2H9V7c0-1.654 1.346-3 3-3s3 1.346 3 3v1h2zm1 4 .002 8H6v-8h12z"></path></svg>    
             </button>
         {/if}
-        {#if $loggedInUser && $loggedInUser.role >= 3}
+        {#if $loggedInUser && $loggedInUser.role >= 4}
             <!-- <div class="h-4"></div> -->
             <button class="btn btn-icon variant-soft-error w-10 h-10" on:click={(e)=>{
                 e.preventDefault();
@@ -182,8 +182,8 @@
                 const modal = {
                     type: 'confirm',
                     // Data
-                    title: 'Please Confirm',
-                    body: 'Are you sure you wish to proceed?',
+                    title: 'ARE YOU SURE U FUCKING DUMBASS?',
+                    body: `Are you sure you want to delete ${project.title}? THIS CANT BE UNDONE`,
                     // TRUE if confirm pressed, FALSE if cancel pressed
                     // @ts-ignore
                     response: (r) => {
