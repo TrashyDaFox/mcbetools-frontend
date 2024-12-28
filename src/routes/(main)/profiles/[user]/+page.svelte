5a796b83366588d08183233fc37b717152f04834
Luna <antspa767@gmail.com>
2024-12-28 06:56:58 -0600

    a

--- src/routes/(main)/profiles/[user]/+page.svelte
+++ src/routes/(main)/profiles/[user]/+page.svelte
@@ -13,6 +13,7 @@
 	import MessageModal from '../../MessageModal.svelte';
 	import badges from '../../../badges.js';
     import { Confetti } from "svelte-confetti"
+	import { loggedInUser } from '../../loggedInUserStore.js';
 
     initializeStores();
     const modalStore = getModalStore();
@@ -184,6 +185,30 @@
                 <span><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/><polyline points="22,6 12,13 2,6"/></svg></span>
                 <span>Message</span>
             </button>
+            {#if $loggedInUser && $loggedInUser.role >= 3}
+                <button class="btn btn-sm w-full max-w-72 variant-ghost-surface" on:click={()=>{
+                    modalStore.trigger({
+                        type: 'confirm',
+                        title: "Are you sure?",
+                        body: "This will make this user the creator of the week (or queue them for the future if theres already one)",
+                        response: (r)=>{
+                            if(r) {
+                                axios.post(`${config.apiEndpoint}/owner/queue-cotw`, {
+                                    user: $profileData.handle
+                                }, {
+                                    headers: {
+                                        Authorization: localStorage.getItem("sessionToken")
+                                    }
+                                })
+                            }
+                        }
+                    })
+                }}>
+                    <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: currentColor;"><path d="M5 8.999c0 1.902.765 3.627 2 4.89V21a.998.998 0 0 0 1.447.895L12 20.118l3.553 1.776a.992.992 0 0 0 .972-.043c.295-.183.475-.504.475-.851v-7.11a6.976 6.976 0 0 0 2-4.891C19 5.14 15.86 2 12 2S5 5.14 5 8.999zm7.447 9.106a1 1 0 0 0-.895 0L9 19.382v-4.067c.911.434 1.926.685 3 .685s2.089-.25 3-.685v4.066l-2.553-1.276zM12 4c2.756 0 5 2.242 5 4.999A5.006 5.006 0 0 1 12 14c-2.757 0-5-2.243-5-5.001A5.005 5.005 0 0 1 12 4z"></path></svg></span>
+                    <span>Queue COTW</span>
+                </button>
+
+            {/if}
             <button class="btn btn-sm w-full max-w-72 {$followedList.includes($profileData.handle) ? "variant-ghost-surface" : "variant-filled"} " on:click={()=>{
                 axios.post(`${config.apiEndpoint}/follow`, {
                     handle: $profileData.handle
