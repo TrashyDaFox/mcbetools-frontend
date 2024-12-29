4241060cd733ac1705cbcbcbeeca33eb8ec04e94
Luna <antspa767@gmail.com>
2024-12-29 01:11:02 -0600

    a

--- src/HeaderWidgets/Yes.svelte
+++ src/HeaderWidgets/Yes.svelte
@@ -1,5 +1,7 @@
 <!-- <button class="btn btn-icon variant-ghost-primary text-primary-500 hidden md:flex"> -->
-<button class="btn btn-icon variant-ghost-surface w-8 h-8 p-2 hidden md:flex">
+<button class="btn btn-icon variant-ghost-surface w-8 h-8 p-2 hidden md:flex" on:click={()=>{
+    location.pathname = '/bookmarks'
+}}>
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: currentColor;"><path d="M19 10.132v-6c0-1.103-.897-2-2-2H7c-1.103 0-2 .897-2 2V22l7-4.666L19 22V10.132z"></path></svg>
 </button>
 <!-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: currentColor;"><path d="M18 2H6c-1.103 0-2 .897-2 2v18l8-4.572L20 22V4c0-1.103-.897-2-2-2zm0 16.553-6-3.428-6 3.428V4h12v14.553z"></path></svg> -->
