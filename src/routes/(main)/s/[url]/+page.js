4361c8408ffcfe834d2f3f8149ea0bf5a95e10b2
Twashy w <122332042+TrashyKitty@users.noreply.github.com>
2025-04-22 04:17:19 -0500

    a

--- src/routes/(main)/s/[url]/+page.js
+++ src/routes/(main)/s/[url]/+page.js
@@ -12,10 +12,13 @@ export async function load({ params }) {
 	try {
 		if(!params.url.startsWith('draft-')) {
 			const response = await axios.get(`${config.apiEndpoint}/proj/${params.url}`);
-
-			const handle = await axios.get(`${config.apiEndpoint}/id-to-handle/${response.data.project.author}`)
-			const profile = await axios.get(`${config.apiEndpoint}/user-profile/${handle.data.handle}`)	
-			return { data: response.data.project, url: params.url, profile: profile.data.userData }; // Adjust the key according to your needs
+			if(!response.data.error) {
+				const handle = await axios.get(`${config.apiEndpoint}/id-to-handle/${response.data.project.author}`)
+				const profile = await axios.get(`${config.apiEndpoint}/user-profile/${handle.data.handle}`)	
+				return { data: response.data.project, url: params.url, profile: profile.data.userData }; // Adjust the key according to your needs	
+			} else {
+				return { data: {}, url: params.url, profile: {} }; // Adjust the key according to your needs
+			}
 		} else {
 			return { data: {}, url: params.url, profile: {} }; // Adjust the key according to your needs
 
