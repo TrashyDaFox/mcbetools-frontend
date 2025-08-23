1e5033a951036e1fa55c017083358526e37520da
Twashy w <122332042+TrashyKitty@users.noreply.github.com>
2025-08-23 01:20:22 -0500

    fix visual bug

--- src/routes/(main)/leaderboards/+page.svelte
+++ src/routes/(main)/leaderboards/+page.svelte
@@ -8,7 +8,7 @@
 </script>
 <div class="w-full h-full bg-surface-800">
     <div class="w-full h-full relative bg-gradient-to-br from-[#54a7ff05] to-[#54a7ff3F]">
-        <img src="/community_particles.png" alt="" class="absolute h-full aspect-square bottom-0 right-0 image-cover" style="pointer-events: none; image-rendering: pixelated;">
+        <img src="/community_particles.png" alt="" class="fixed h-screen aspect-square bottom-0 right-0 image-cover" style="pointer-events: none; image-rendering: pixelated;">
         <div class="p-4 w-full">
             <div class="flex items-center flex-col justify-center w-full">
                 <h1 class="h1 text-[#54a7ff] font-bold flex items-center gap-4">Leaderboards <span class="badge variant-filled-primary !bg-[#54a7ff] mt-2">EARLY ALPHA</span></h1>
@@ -20,7 +20,7 @@
                     <div class="w-full bg-surface-900/10 border-b-[1px] border-surface-500">
                         <TabGroup>
                             <Tab bind:group={tab} name="tab1" value={0}>
-                                Creators
+                                Creator Points
                             </Tab>
                             <Tab bind:group={tab} name="tab1" value={1}>
                                 Projects
