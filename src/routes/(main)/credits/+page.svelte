4ae99f57c28f24631918fc7ecdb7e71e7e983a14
Twashy w <122332042+TrashyKitty@users.noreply.github.com>
2025-09-04 21:40:21 -0500

    SAF(HE$@#*O

---
+++ src/routes/(main)/credits/+page.svelte
@@ -0,0 +1,19 @@
+<div class="p-4">
+    <div class="flex w-full items-center justify-center gap-1 flex-col">
+        <h1 class="h3 fancy-title2">MCBETools Credits</h1>
+        <p>People who helped contribute to the MCBETools codebase will go here!</p>
+
+    </div>
+    <div class="h-4"></div>
+    <div class="flex gap-4">
+        <div class="card overflow-hidden bg-initial p-4">
+            <div class="rounded-container-token overflow-hidden" style="background: url(/apps/asstoy.png); background-size:cover;background-position:center;">
+                <div class="card variant-glass-surface pt-8 px-8">
+                    <img src="/trashyy.png" alt="" class="max-h-96">
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
--- src/routes/(main)/guidelines/+page.svelte
+++ src/routes/(main)/guidelines/+page.svelte
@@ -1,3 +1,21 @@
+<script lang="ts">
+	import axios from "axios";
+	import { writable } from "svelte/store";
+	import config from "../../config";
+
+    let guidelines = writable([
+        // ["No NSFW!", "Please save our eyes, and don't upload NSFW."],
+        // ["No impersonation", "Impersonation of creators on MCPEDL and other services on MCBETools is prohibited. This will result in your account being disabled until looked into further."],
+        // ["No discrimination", "Any forms of discrimination (such as racism, sexism, transphobia, homophobia, etc.) are strictly prohibited. Doing so publicly on this platform will get you banned if caught."],
+        // ["No underage users", "MCBETools strictly enforces the underage laws in all countries for Social Media. We hope to add child protection or parental controls in the future. Research these laws, but in the US, the general minimum age is 13 years old."],
+        // ["Keep the platform pg13.", "Minecraft is a vast community, with loads of players streching from 5 to 30 year olds and we can't ban all of the underage users, so please keep swearing and arguing to a minimum."],
+    ])
+
+    axios.get(`${config.apiEndpoint}/guidelines`).then(res=>{
+        guidelines.set(res.data);
+    })
+</script>
+
 <div class="w-full p-4 flex items-center justify-center">
     <img src="/mcbetoolsguidelines.png" class="max-w-full h-auto object-cover" alt="">
 </div>
@@ -20,44 +38,21 @@
 </div>
 
 <div class="p-4">
-    <div class="card p-4">
-        <h3 class="h3 font-bold">No NSFW!</h3>
-        <div class="h-2"></div>
-        <hr>
-        <div class="h-2"></div>
-        <p class="opacity-50">Please save our eyes, and don't upload NSFW.</p>
-    </div>
-    <div class="h-4"></div>
-    <div class="card p-4">
-        <h3 class="h3 font-bold">No impersonation</h3>
-        <div class="h-2"></div>
-        <hr>
-        <div class="h-2"></div>
-        <p class="opacity-50">Impersonation of creators on MCPEDL and other services on MCBETools is prohibited. This will result in your account being disabled until looked into further.</p>
-    </div>
-    <div class="h-4"></div>
-    <div class="card p-4">
-        <h3 class="h3 font-bold">No discrimination</h3>
-        <div class="h-2"></div>
-        <hr>
-        <div class="h-2"></div>
-        <p class="opacity-50">Any forms of discrimination (such as racism, sexism, transphobia, homophobia, etc.) are strictly prohibited.</p>
-    </div>
-    <div class="h-4"></div>
-    <div class="card p-4">
-        <h3 class="h3 font-bold">No underage users</h3>
-        <div class="h-2"></div>
-        <hr>
-        <div class="h-2"></div>
-        <p class="opacity-50">MCBETools strictly enforces the underage laws in all countries for Social Media. We hope to add child protection or parental controls in the future. Research these laws, but in the US, the general minimum age is 13 years old.</p>
-    </div>
-    <div class="h-4"></div>
+    {#if $guidelines && $guidelines.length}
+        {#each $guidelines as guideline}
+            <div class="card p-4">
+                <h3 class="h3 font-bold">{guideline[0]}</h3>
+                <div class="h-2"></div>
+                <hr>
+                <div class="h-2"></div>
+                <p class="opacity-50">{guideline[1]}</p>
+            </div>
+            <div class="h-4"></div>
+        {/each}
+    {:else}
+        <div class="w-full py-12 flex items-center justify-center">
+            <div class="loader max-w-screen w-96"></div>
 
-    <div class="card p-4">
-        <h3 class="h3 font-bold">Keep the platform friendly for 13 year olds.</h3>
-        <div class="h-2"></div>
-        <hr>
-        <div class="h-2"></div>
-        <p class="opacity-50">Minecraft is a vast community, with loads of players streching from 5 to 30 year olds and we can't ban all of the underage users, so please keep swearing and arguing to a minimum.</p>
-    </div>
+        </div>
+    {/if}
 </div>
